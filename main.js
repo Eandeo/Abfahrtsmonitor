ddab416b4adcd8725d433b3289cdf5fdab13d22b
eandeo <andi1220@hotmail.com>
2023-05-11 22:09:22 +0200

    still cors error but added coding train iss tutorial

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-function APIrequest(){
+function APIrequest() {
 	const stopId = document.getElementById("haltepunkt").value;
 	console.log(stopId);
-	
-	populate(stopId)
+
+	getHaltepunkt(stopId)
 
 
 }
 
-async function populate(stopId) {
-			const requestURL = 'https://www.wienerlinien.at/ogd_realtime/monitor?stopID=' + stopId;
 async function getHaltepunkt(stopId) {
-	const response = await fetch(requestURL);
+	const requestURL = `https://www.wienerlinien.at/ogd_realtime/monitor?stopID=${stopId}`;
+	const requestHeaders = {
+	  'Accept': 'application/json',
+	  'Content-Type': 'application/json'
+	};
+	const response = await fetch(requestURL, { method: 'GET', headers: requestHeaders });
 	const stop = await response.json();
 	console.log(stop);
+  }
+ 
+  
+  const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
+  async function getISS(){
+	const response = await fetch(api_url);
+	const data = await response.json();
+	console.log(data.name);
+  }
+
+  getISS();
 
 
-	}
